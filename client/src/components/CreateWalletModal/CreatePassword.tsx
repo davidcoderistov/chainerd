aa7dda73059c1421d45fe7d3a55717139a262ef6
davidcoderistov <davidcoderistov@gmail.com>
2022-05-28 15:32:23 +0200

    Create-Wallet/Create initial version of CreatePassword component

---
+++ client/src/components/CreateWalletModal/CreatePassword.tsx
@@ -0,0 +1,102 @@
+import React, { useState } from 'react'
+import { Grid } from '@mui/material'
+import {
+    FormControl,
+    InputLabel,
+    OutlinedInput,
+    InputAdornment,
+    IconButton,
+    Button,
+} from '@mui/material'
+import { Typography } from '@mui/material'
+import Visibility from '@mui/icons-material/Visibility'
+import VisibilityOff from '@mui/icons-material/VisibilityOff'
+
+interface CreatePasswordProps {
+    onCreate: () => void
+}
+
+
+export default function CreatePassword({ onCreate } : CreatePasswordProps) {
+
+    const [password, setPassword] = useState<string>('')
+    const [showPassword, setShowPassword] = useState<boolean>(false)
+
+    const [confirmPassword, setConfirmPassword] = useState<string>('')
+    const [showConfirmPassword, setShowConfirmPassword] = useState<boolean>(false)
+
+    const handleChangePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(event.target.value)
+    }
+
+    const handleClickShowPassword = () => {
+        setShowPassword(!showPassword)
+    }
+
+    const handleChangeConfirmPassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setConfirmPassword(event.target.value)
+    }
+
+    const handleClickShowConfirmPassword = () => {
+        setShowConfirmPassword(!showConfirmPassword)
+    }
+
+    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+    }
+
+    return (
+        <Grid item xs={6}>
+            <Typography variant='h4'>
+                Create Password
+            </Typography>
+            <FormControl fullWidth variant="outlined" sx={{ mt: 5 }}>
+                <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                <OutlinedInput
+                    id="outlined-adornment-password"
+                    type={showPassword ? 'text' : 'password'}
+                    value={password}
+                    onChange={handleChangePassword}
+                    endAdornment={
+                        <InputAdornment position="end">
+                            <IconButton
+                                aria-label="toggle password visibility"
+                                onClick={handleClickShowPassword}
+                                onMouseDown={handleMouseDownPassword}
+                                edge="end"
+                            >
+                                {showPassword ? <VisibilityOff /> : <Visibility />}
+                            </IconButton>
+                        </InputAdornment>
+                    }
+                    label="Password"
+                />
+            </FormControl>
+            <FormControl fullWidth variant="outlined" sx={{ mt: 2 }}>
+                <InputLabel htmlFor="outlined-adornment-password">Confirm Password</InputLabel>
+                <OutlinedInput
+                    id="outlined-adornment-confirm-password"
+                    type={showConfirmPassword ? 'text' : 'password'}
+                    value={confirmPassword}
+                    onChange={handleChangeConfirmPassword}
+                    endAdornment={
+                        <InputAdornment position="end">
+                            <IconButton
+                                aria-label="toggle password visibility"
+                                onClick={handleClickShowConfirmPassword}
+                                onMouseDown={handleMouseDownPassword}
+                                edge="end"
+                            >
+                                {showConfirmPassword ? <VisibilityOff /> : <Visibility />}
+                            </IconButton>
+                        </InputAdornment>
+                    }
+                    label="Confirm Password"
+                />
+            </FormControl>
+            <Button variant='contained' sx={{ mt: 5, borderRadius: 8, px: 4 }} onClick={onCreate}>
+                Create
+            </Button>
+        </Grid>
+    )
+}
