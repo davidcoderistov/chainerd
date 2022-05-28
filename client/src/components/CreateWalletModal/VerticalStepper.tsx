dac2f4f557ea160a7138b778b9c5c7a63acdaf2b
davidcoderistov <davidcoderistov@gmail.com>
2022-05-28 12:43:31 +0200

    Create-Wallet/Create VerticalStepper component

---
+++ client/src/components/CreateWalletModal/VerticalStepper.tsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import { Avatar, Typography, Divider } from '@mui/material'
+import { CheckCircle } from '@mui/icons-material'
+import { styled } from '@mui/material'
+
+
+const Container = styled('div')({
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    rowGap: '10px',
+    columnGap: '10px',
+    height: '100%',
+})
+
+const Column = styled('div')({
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    rowGap: '5px',
+})
+
+const StyledAvatar = styled(Avatar)({
+    width: '25px',
+    height: '25px',
+})
+
+const StyledDivider = styled(Divider)({
+    flex: '1 1 auto',
+})
+
+const steps = [
+    'Create Password',
+    'Seed Info',
+    'Create Wallet',
+]
+
+export default function VerticalStepper () {
+    const activeStep = 0
+    return (
+        <Container>
+            { steps.map((step, index) => (
+                <React.Fragment key={index}>
+                    <Column>
+                        <StyledAvatar
+                            sx={{
+                                bgcolor: index <= activeStep ? 'primary.main' : 'grey'
+                        }}>
+                            { index < activeStep ? (
+                                <CheckCircle/>
+                            ) : index + 1 }
+                        </StyledAvatar>
+                        <Typography
+                            variant='overline'
+                            sx={{
+                                fontWeight: activeStep === index ? '600' : 'normal',
+                                color: index !== activeStep ? 'gray' : undefined
+                            }}>
+                            { step }
+                        </Typography>
+                    </Column>
+                    <StyledDivider orientation='vertical'/>
+                </React.Fragment>
+            ))}
+        </Container>
+    )
+}
