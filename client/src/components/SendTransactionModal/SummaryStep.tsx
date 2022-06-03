da8cc7918b603315f026a7db683d2bcaaa934593
davidcoderistov <davidcoderistov@gmail.com>
2022-06-03 18:10:40 +0200

    Send-Transaction/Create SummaryStep component

---
+++ client/src/components/SendTransactionModal/SummaryStep.tsx
@@ -0,0 +1,116 @@
+import React from 'react'
+import Label from '../Label'
+import { Grid, Divider, Typography, styled } from '@mui/material'
+import { GridView } from '@mui/icons-material'
+
+const AddressContainer = styled(Grid)({
+    display: 'flex',
+    flexDirection: 'row',
+    columnGap: '15px',
+})
+
+const AddressDivider = styled('div')({
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    rowGap: '10px',
+    paddingTop: '5px',
+    paddingBottom: '8px',
+})
+
+const GridViewIcon = styled(GridView)({
+    borderRadius: '50%',
+    padding: '7px',
+    backgroundColor: 'rgb(239,244,254)',
+})
+
+const InfoContainer = styled(Grid)({
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+})
+
+const InfoContent = styled('div')({
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'end',
+})
+
+const Info = ({ title, subtitle, caption } : { title: string, subtitle: string, caption: string }) => (
+    <InfoContainer item xs={12}>
+        <Label value={title} />
+        <InfoContent>
+            <Typography variant='subtitle2'>
+                { subtitle }
+            </Typography>
+            <Label value={caption} />
+        </InfoContent>
+    </InfoContainer>
+)
+
+export interface SummaryStepProps {
+    fromAddress: string,
+    toAddress: string,
+    cryptoWithdrawAmount: string,
+    fiatWithdrawAmount: string,
+    cryptoFees: string,
+    fiatFees: string,
+    cryptoTotalAmount: string,
+    fiatTotalAmount: string,
+}
+
+export default function SummaryStep (props: SummaryStepProps) {
+
+    const {
+        fromAddress,
+        toAddress,
+        cryptoWithdrawAmount,
+        fiatWithdrawAmount,
+        cryptoFees,
+        fiatFees,
+        cryptoTotalAmount,
+        fiatTotalAmount
+    } = props
+
+    return (
+        <React.Fragment>
+            <AddressContainer item xs={12}>
+                <AddressDivider>
+                    <GridViewIcon
+                        fontSize='small'
+                        color='primary' />
+                    <Divider
+                        orientation='vertical'
+                        sx={{ flex: '1 1 auto' }} />
+                    <GridViewIcon
+                        fontSize='small'
+                        color='primary' />
+                </AddressDivider>
+                <div>
+                    <Label value='From'/>
+                    <Typography variant='subtitle1' sx={{ mb: 2 }}>
+                        { fromAddress }
+                    </Typography>
+                    <Label value='To'/>
+                    <Typography variant='subtitle1'>
+                        { toAddress }
+                    </Typography>
+                </div>
+            </AddressContainer>
+            <Divider sx={{ mt: 1, mb: 2 }}/>
+            <Info
+                title='Amount'
+                subtitle={cryptoWithdrawAmount}
+                caption={fiatWithdrawAmount} />
+            <Info
+                title='Network Fees'
+                subtitle={cryptoFees}
+                caption={fiatFees} />
+            <Divider sx={{ my: 1 }}/>
+            <Info
+                title='Total to debit'
+                subtitle={cryptoTotalAmount}
+                caption={fiatTotalAmount} />
+        </React.Fragment>
+    )
+}
