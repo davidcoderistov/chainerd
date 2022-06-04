11b3809591d5d54148b015de39471a225f5e6ce7
davidcoderistov <davidcoderistov@gmail.com>
2022-06-04 12:18:10 +0200

    Create-Wallet/Redesign CreatePassword component

--- client/src/components/CreateWalletModal/CreatePassword.tsx
+++ client/src/components/CreateWalletModal/CreatePassword.tsx
@@ -1,23 +1,11 @@
 import React, { useState } from 'react'
-import { Grid } from '@mui/material'
-import {
-    FormControl,
-    InputLabel,
-    OutlinedInput,
-    InputAdornment,
-    IconButton,
-    Button,
-} from '@mui/material'
-import { Typography } from '@mui/material'
+import TextInput from '../TextInput'
+import { InputAdornment, IconButton } from '@mui/material'
 import Visibility from '@mui/icons-material/Visibility'
 import VisibilityOff from '@mui/icons-material/VisibilityOff'
 
-interface CreatePasswordProps {
-    onCreate: (password: string) => void
-}
 
-
-export default function CreatePassword({ onCreate } : CreatePasswordProps) {
+export default function CreatePassword () {
 
     const [password, setPassword] = useState<string>('')
     const [showPassword, setShowPassword] = useState<boolean>(false)
@@ -45,45 +33,39 @@ export default function CreatePassword({ onCreate } : CreatePasswordProps) {
         event.preventDefault();
     }
 
-    const handleOnCreate = () => {
-        onCreate(password)
-    }
-
     return (
-        <Grid item xs={6}>
-            <Typography variant='h4'>
-                Create Password
-            </Typography>
-            <FormControl fullWidth variant="outlined" sx={{ mt: 5 }}>
-                <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-                <OutlinedInput
-                    id="outlined-adornment-password"
-                    type={showPassword ? 'text' : 'password'}
-                    value={password}
-                    onChange={handleChangePassword}
-                    endAdornment={
+        <React.Fragment>
+            <TextInput
+                id='outlined-adornment-password'
+                inputLabel='Password'
+                placeholder='Enter Password'
+                type={showPassword ? 'text' : 'password'}
+                value={password}
+                onChange={handleChangePassword}
+                InputProps={{
+                    endAdornment: (
                         <InputAdornment position="end">
                             <IconButton
                                 aria-label="toggle password visibility"
                                 onClick={handleClickShowPassword}
                                 onMouseDown={handleMouseDownPassword}
-                                edge="end"
-                            >
+                                edge="end">
                                 {showPassword ? <VisibilityOff /> : <Visibility />}
                             </IconButton>
                         </InputAdornment>
-                    }
-                    label="Password"
-                />
-            </FormControl>
-            <FormControl fullWidth variant="outlined" sx={{ mt: 2 }}>
-                <InputLabel htmlFor="outlined-adornment-password">Confirm Password</InputLabel>
-                <OutlinedInput
-                    id="outlined-adornment-confirm-password"
-                    type={showConfirmPassword ? 'text' : 'password'}
-                    value={confirmPassword}
-                    onChange={handleChangeConfirmPassword}
-                    endAdornment={
+                    )
+                }}
+                sx={{ mb: 2 }}
+                fullWidth />
+            <TextInput
+                id='outlined-adornment-confirm-password'
+                placeholder='Enter Confirm Password'
+                inputLabel='Confirm Password'
+                type={showConfirmPassword ? 'text' : 'password'}
+                value={confirmPassword}
+                onChange={handleChangeConfirmPassword}
+                InputProps={{
+                    endAdornment: (
                         <InputAdornment position="end">
                             <IconButton
                                 aria-label="toggle password visibility"
@@ -94,13 +76,9 @@ export default function CreatePassword({ onCreate } : CreatePasswordProps) {
                                 {showConfirmPassword ? <VisibilityOff /> : <Visibility />}
                             </IconButton>
                         </InputAdornment>
-                    }
-                    label="Confirm Password"
-                />
-            </FormControl>
-            <Button variant='contained' sx={{ mt: 5, borderRadius: 8, px: 4 }} onClick={handleOnCreate}>
-                Create
-            </Button>
-        </Grid>
+                    )
+                }}
+                fullWidth />
+        </React.Fragment>
     )
 }
