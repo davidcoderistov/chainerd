f6b3ddeb9cbcfc5b368beff7d020e3da35041a9b
davidcoderistov <davidcoderistov@gmail.com>
2022-06-15 18:13:34 +0200

    Create-Wallet/Add getErrorCode selector

--- client/src/selectors/keystore.ts
+++ client/src/selectors/keystore.ts
@@ -5,11 +5,14 @@ const getKeystore = (state: RootState) => state.keystore.keystore
 
 const getError = (state: RootState) => state.keystore.error
 
+const getErrorCode = (state: RootState) => state.keystore.errorCode
+
 const getLoading = (state: RootState) => state.keystore.loading
 
 
 export {
     getKeystore,
     getError,
+    getErrorCode,
     getLoading,
 }
