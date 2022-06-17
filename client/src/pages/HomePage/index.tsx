c17e5fb7af9af478b276875fb6c9b9a158de65f0
davidcoderistov <davidcoderistov@gmail.com>
2022-06-17 11:19:40 +0200

    Inject all modals into HomePage component

--- client/src/pages/HomePage/index.tsx
+++ client/src/pages/HomePage/index.tsx
@@ -1,8 +1,13 @@
-import React, { useEffect, useMemo } from 'react'
-import { useDispatch } from 'react-redux'
+import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { createWallet } from '../../slices/keystore'
+import { getKeystore } from '../../selectors/keystore'
 import { Box } from '@mui/material'
 import AppToolbar from '../../components/AppToolbar'
+import CreateWalletModal from '../../components/CreateWalletModal'
+import RestoreWalletModal from '../../components/RestoreWalletModal'
+import SendTransactionModal from '../../components/SendTransactionModal'
+import CloseWalletModal from '../../components/CloseWalletModal'
 import store from 'store'
 
 
@@ -12,22 +17,79 @@ export default function HomePage () {
 
     const dispatch = useDispatch()
 
-    const keystore = useMemo(() => store.get('keystore'), [])
+    const keystore = useSelector(getKeystore)
 
     useEffect(() => {
+        const keystore = store.get('keystore')
         if (keystore) {
             dispatch(createWallet.load({ keystore }))
         }
-    }, [keystore])
+    }, [])
+
+    const [isCreateWalletModalOpen, setIsCreateWalletModalOpen] = useState<boolean>(false)
+
+    const openCreateWalletModal = () => {
+        setIsCreateWalletModalOpen(true)
+    }
+
+    const closeCreateWalletModal = () => {
+        setIsCreateWalletModalOpen(false)
+    }
+
+    const [isRestoreWalletModalOpen, setIsRestoreWalletModalOpen] = useState<boolean>(false)
+
+    const openRestoreWalletModal = () => {
+        setIsRestoreWalletModalOpen(true)
+    }
+
+    const closeRestoreWalletModal = () => {
+        setIsRestoreWalletModalOpen(false)
+    }
+
+    const [isSendTransactionModalOpen, setIsSendTransactionModalOpen] = useState<boolean>(false)
+
+    const openSendTransactionModal = () => {
+        setIsSendTransactionModalOpen(true)
+    }
+
+    const closeSendTransactionModal = () => {
+        setIsSendTransactionModalOpen(false)
+    }
+
+    const [isCloseWalletModalOpen, setIsCloseWalletModalOpen] = useState<boolean>(false)
+
+    const openCloseWalletModal = () => {
+        setIsCloseWalletModalOpen(true)
+    }
+
+    const closeCloseWalletModal = () => {
+        setIsCloseWalletModalOpen(false)
+    }
 
     return (
         <Box>
             <AppToolbar
                 walletExists={!!keystore}
-                onCreateWallet={() => console.log('onCreateWallet()')}
-                onRestoreWallet={() => console.log('onRestoreWallet()')}
-                onSendTransaction={() => console.log('onSendTransaction()')}
-                onCloseWallet={() => console.log('onCloseWallet()')} />
+                onCreateWallet={openCreateWalletModal}
+                onRestoreWallet={openRestoreWalletModal}
+                onSendTransaction={openSendTransactionModal}
+                onCloseWallet={openCloseWalletModal} />
+            <CreateWalletModal
+                open={isCreateWalletModalOpen}
+                onCreateWallet={closeCreateWalletModal}
+                onClose={closeCreateWalletModal} />
+            <RestoreWalletModal
+                open={isRestoreWalletModalOpen}
+                onRestoreWallet={closeRestoreWalletModal}
+                onClose={closeRestoreWalletModal} />
+            <SendTransactionModal
+                open={isSendTransactionModalOpen}
+                onClose={closeSendTransactionModal}
+                onConfirm={closeSendTransactionModal} />
+            <CloseWalletModal
+                open={isCloseWalletModalOpen}
+                onCloseModal={closeCloseWalletModal}
+                onCloseWallet={closeCloseWalletModal} />
         </Box>
     )
 }
