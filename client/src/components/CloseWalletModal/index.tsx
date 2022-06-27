2ac6e17f8ff6fa7d2c3b2c7294d031935f1d99a2
davidcoderistov <davidcoderistov@gmail.com>
2022-06-27 12:27:15 +0200

    Remove snackbar from CloseWalletModal component

--- client/src/components/CloseWalletModal/index.tsx
+++ client/src/components/CloseWalletModal/index.tsx
@@ -1,7 +1,6 @@
-import React, { useState, useCallback } from 'react'
+import React, { useCallback } from 'react'
 import { Dialog, DialogTitle, DialogContent, DialogActions } from '@mui/material'
 import { Grid, Typography, Button, IconButton, styled } from '@mui/material'
-import Snackbar from '../Snackbar'
 import { Close } from '@mui/icons-material'
 
 const DialogTitleStyled = styled(DialogTitle)({
@@ -18,57 +17,43 @@ interface CloseWalletModalProps {
 
 export default function CloseWalletModal ({ open, onCloseModal, onCloseWallet }: CloseWalletModalProps) {
 
-    const [showSnackbar, setShowSnackbar] = useState<boolean>(false)
-
     const handleOnConfirm = useCallback( () => {
-        setShowSnackbar(true)
         onCloseWallet()
     }, [onCloseWallet])
 
-    const handleCloseSnackbar = () => {
-        setShowSnackbar(false)
-    }
-
     return (
-        <React.Fragment>
-            <Dialog open={open} fullWidth={true} maxWidth='xs' scroll='paper'>
-                <DialogTitleStyled>
-                    <div/>
-                    <div>Close Wallet</div>
-                    <IconButton
-                        aria-label='close'
-                        onClick={onCloseModal}
-                        sx={{
-                            color: (theme) => theme.palette.grey[500],
-                        }}
-                    >
-                        <Close />
-                    </IconButton>
-                </DialogTitleStyled>
-                <DialogContent dividers={true}>
-                    <Grid container spacing={2}>
-                        <Grid item xs={12}>
-                            <Typography variant='body1'>
-                                You are about to completely remove this wallet and its associated data from the browser.
-                                Are you sure you want to proceed ?
-                            </Typography>
-                        </Grid>
+        <Dialog open={open} fullWidth={true} maxWidth='xs' scroll='paper'>
+            <DialogTitleStyled>
+                <div/>
+                <div>Close Wallet</div>
+                <IconButton
+                    aria-label='close'
+                    onClick={onCloseModal}
+                    sx={{
+                        color: (theme) => theme.palette.grey[500],
+                    }}
+                >
+                    <Close />
+                </IconButton>
+            </DialogTitleStyled>
+            <DialogContent dividers={true}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <Typography variant='body1'>
+                            You are about to completely remove this wallet and its associated data from the browser.
+                            Are you sure you want to proceed ?
+                        </Typography>
                     </Grid>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={onCloseModal}>
-                        No
-                    </Button>
-                    <Button sx={{ mr: 1 }} onClick={handleOnConfirm}>
-                        Yes
-                    </Button>
-                </DialogActions>
-            </Dialog>
-            <Snackbar
-                open={showSnackbar}
-                error={false}
-                message='Wallet successfully closed'
-                onClose={handleCloseSnackbar} />
-        </React.Fragment>
+                </Grid>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={onCloseModal}>
+                    No
+                </Button>
+                <Button sx={{ mr: 1 }} onClick={handleOnConfirm}>
+                    Yes
+                </Button>
+            </DialogActions>
+        </Dialog>
     )
 }
