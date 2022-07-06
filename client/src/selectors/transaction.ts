bf378f0d5c62d2f02bcc9a89301876737e1e9b97
davidcoderistov <davidcoderistov@gmail.com>
2022-07-06 22:25:46 +0200

    Add gas info selectors

--- client/src/selectors/transaction.ts
+++ client/src/selectors/transaction.ts
@@ -1,13 +1,23 @@
 import { RootState } from '../app/store'
+import { roundedWeiToGwei } from '../utils'
 
 const getEthAmount = (state: RootState): string => state.transaction.ethAmount
 
 const getFiatAmount = (state: RootState): string => state.transaction.fiatAmount
+
+const getLowGasPrice = (state: RootState): number => roundedWeiToGwei(state.transaction.gasInfo.lowGasPrice)
+
+const getHighGasPrice = (state: RootState): number => roundedWeiToGwei(state.transaction.gasInfo.highGasPrice)
+
+const getGasPrice = (state: RootState): number => state.transaction.gasInfo.gasPrice
 
 const getLoading = (state: RootState): boolean => state.transaction.request.loading
 
 export {
     getEthAmount,
     getFiatAmount,
+    getLowGasPrice,
+    getHighGasPrice,
+    getGasPrice,
     getLoading
 }
