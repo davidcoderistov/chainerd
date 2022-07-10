a1b34218839900dce7001b34255ecb2d30466201
davidcoderistov <davidcoderistov@gmail.com>
2022-07-10 11:12:08 +0200

    Handle send transaction in SendTransactionModal component

--- client/src/components/SendTransactionModal/index.tsx
+++ client/src/components/SendTransactionModal/index.tsx
@@ -22,6 +22,7 @@ import { Close } from '@mui/icons-material'
 import RecipientStep from './RecipientStep'
 import AmountStep, { AmountStepProps } from './AmountStep'
 import SummaryStep from './SummaryStep'
+import ConfirmPasswordModal from '../ConfirmPasswordModal'
 import { toRoundedEth, toRoundedFiat } from '../../utils'
 import { ethers } from 'ethers'
 
@@ -84,11 +85,26 @@ export default function SendTransactionModal ({ open, onClose, onConfirm } : Sen
     const ethTotal = useSelector(getEthTotalAmount)
     const fiatTotal = useSelector(getFiatTotalAmount)
 
+    const [isConfirmPasswordModalOpen, setIsConfirmPasswordModalOpen] = useState<boolean>(false)
+
+    const handleCloseConfirmPasswordModal = () => {
+        setIsConfirmPasswordModalOpen(false)
+    }
+
+    const handleConfirmPassword = (password: string) => {
+        setIsConfirmPasswordModalOpen(false)
+        dispatch(transactionActions.sendTransaction({
+            fromAddress,
+            toAddress,
+            password,
+        }))
+    }
+
     const handleOnContinue = () => {
         if (activeStep < 2) {
             setActiveStep(activeStep + 1)
         } else {
-            onConfirm()
+            setIsConfirmPasswordModalOpen(true)
         }
     }
 
@@ -97,76 +113,82 @@ export default function SendTransactionModal ({ open, onClose, onConfirm } : Sen
             (addresses.length <= 0 || !ethers.utils.isAddress(toAddress))
 
     return (
-        <Dialog open={open} fullWidth maxWidth='sm' scroll='paper'>
-            <DialogTitleStyled>
-                <div/>
-                <div>Send</div>
-                <IconButton
-                    aria-label='close'
-                    sx={{
-                        color: (theme) => theme.palette.grey[500],
-                    }}
-                    onClick={onClose}
-                >
-                    <Close />
-                </IconButton>
-            </DialogTitleStyled>
-            <DialogContent dividers={true}>
-                <div style={{ height: '400px' }}>
-                    <Grid container spacing={2}>
-                        <Grid item xs={12}>
-                            <Stepper activeStep={activeStep} alternativeLabel>
-                                {steps.map((label) => (
-                                    <Step key={label}>
-                                        <StepLabel>{ label }</StepLabel>
-                                    </Step>
-                                ))}
-                            </Stepper>
+        <React.Fragment>
+            <Dialog open={open} fullWidth maxWidth='sm' scroll='paper'>
+                <DialogTitleStyled>
+                    <div/>
+                    <div>Send</div>
+                    <IconButton
+                        aria-label='close'
+                        sx={{
+                            color: (theme) => theme.palette.grey[500],
+                        }}
+                        onClick={onClose}
+                    >
+                        <Close />
+                    </IconButton>
+                </DialogTitleStyled>
+                <DialogContent dividers={true}>
+                    <div style={{ height: '400px' }}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <Stepper activeStep={activeStep} alternativeLabel>
+                                    {steps.map((label) => (
+                                        <Step key={label}>
+                                            <StepLabel>{ label }</StepLabel>
+                                        </Step>
+                                    ))}
+                                </Stepper>
+                            </Grid>
+                            <Grid item xs={12} sx={{ mt: 1, mx: 1 }}>
+                                { activeStep === 0 && (
+                                    <RecipientStep
+                                        addresses={showAddresses}
+                                        fromAddress={fromAddress}
+                                        onChangeFromAddress={setFromAddress}
+                                        toAddress={toAddress}
+                                        onChangeToAddress={setToAddress} />
+                                )}
+                                { activeStep === 1 && (
+                                    <AmountStep
+                                        cryptoAmount={ethAmount}
+                                        onChangeCryptoAmount={handleChangeEthAmount}
+                                        fiatAmount={fiatAmount}
+                                        onChangeFiatAmount={handleChangeFiatAmount}
+                                        lowGasPrice={lowGasPrice}
+                                        highGasPrice={highGasPrice}
+                                        gasPrice={gasPrice}
+                                        onChangeGasPrice={handleChangeGasPrice} />
+                                )}
+                                { activeStep === 2 && (
+                                    <SummaryStep
+                                        fromAddress={fromAddress}
+                                        toAddress={toAddress}
+                                        cryptoWithdrawAmount={ethAmount}
+                                        fiatWithdrawAmount={fiatAmount}
+                                        cryptoFees={toRoundedEth(ethFee)}
+                                        fiatFees={toRoundedFiat(fiatFee)}
+                                        cryptoTotalAmount={toRoundedEth(ethTotal)}
+                                        fiatTotalAmount={toRoundedFiat(fiatTotal)}  />
+                                )}
+                            </Grid>
                         </Grid>
-                        <Grid item xs={12} sx={{ mt: 1, mx: 1 }}>
-                            { activeStep === 0 && (
-                                <RecipientStep
-                                    addresses={showAddresses}
-                                    fromAddress={fromAddress}
-                                    onChangeFromAddress={setFromAddress}
-                                    toAddress={toAddress}
-                                    onChangeToAddress={setToAddress} />
-                            )}
-                            { activeStep === 1 && (
-                                <AmountStep
-                                    cryptoAmount={ethAmount}
-                                    onChangeCryptoAmount={handleChangeEthAmount}
-                                    fiatAmount={fiatAmount}
-                                    onChangeFiatAmount={handleChangeFiatAmount}
-                                    lowGasPrice={lowGasPrice}
-                                    highGasPrice={highGasPrice}
-                                    gasPrice={gasPrice}
-                                    onChangeGasPrice={handleChangeGasPrice} />
-                            )}
-                            { activeStep === 2 && (
-                                <SummaryStep
-                                    fromAddress={fromAddress}
-                                    toAddress={toAddress}
-                                    cryptoWithdrawAmount={ethAmount}
-                                    fiatWithdrawAmount={fiatAmount}
-                                    cryptoFees={toRoundedEth(ethFee)}
-                                    fiatFees={toRoundedFiat(fiatFee)}
-                                    cryptoTotalAmount={toRoundedEth(ethTotal)}
-                                    fiatTotalAmount={toRoundedFiat(fiatTotal)}  />
-                            )}
-                        </Grid>
-                    </Grid>
-                </div>
-            </DialogContent>
-            <DialogActions>
-                <LoadingButton
-                    sx={{ mr:1 }}
-                    disabled={buttonDisabled}
-                    loading={loading}
-                    onClick={handleOnContinue}>
+                    </div>
+                </DialogContent>
+                <DialogActions>
+                    <LoadingButton
+                        sx={{ mr:1 }}
+                        disabled={buttonDisabled}
+                        loading={loading}
+                        onClick={handleOnContinue}>
                         { activeStep < 2 ? 'Continue' : 'Confirm' }
-                </LoadingButton>
-            </DialogActions>
-        </Dialog>
+                    </LoadingButton>
+                </DialogActions>
+            </Dialog>
+            <ConfirmPasswordModal
+                open={isConfirmPasswordModalOpen}
+                onClose={handleCloseConfirmPasswordModal}
+                onConfirm={handleConfirmPassword} />
+        </React.Fragment>
     )
 }
