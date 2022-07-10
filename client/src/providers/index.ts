f153a2976c81751a020ec570b25ba88905303876
davidcoderistov <davidcoderistov@gmail.com>
2022-07-10 11:48:12 +0200

    Rename api key

--- client/src/providers/index.ts
+++ client/src/providers/index.ts
@@ -1,8 +1,8 @@
 import { ethers } from 'ethers'
 
-const INFURA_API_KEYS = process.env.REACT_APP_INFURA_API_KEYS
+const INFURA_API_KEY = process.env.REACT_APP_INFURA_API_KEY
 
-const ethersProvider = new ethers.providers.JsonRpcProvider(`https://kovan.infura.io/v3/${INFURA_API_KEYS}`)
+const ethersProvider = new ethers.providers.JsonRpcProvider(`https://kovan.infura.io/v3/${INFURA_API_KEY}`)
 
 export {
     ethersProvider
