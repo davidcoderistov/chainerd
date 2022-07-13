8b55dd1c958a84a2e130d43ce8e41e9d416f079b
davidcoderistov <davidcoderistov@gmail.com>
2022-07-13 12:21:47 +0200

    Add default error message method

--- client/src/utils/misc.ts
+++ client/src/utils/misc.ts
@@ -7,3 +7,13 @@ export function getAxiosErrorMessage (error: any, defaultMessage: string): strin
 export function getGenericErrorMessage (error: any, defaultMessage: string): string {
     return error && error.message ? error.message : defaultMessage
 }
+
+export function getErrorMessage (error: any, defaultMessage: string): string {
+    if (error && error.message) {
+        return error.message
+    } else if (error && error.response && error.response.data && error.response.data.error) {
+        return error.response.data.error
+    } else {
+        return defaultMessage
+    }
+}
