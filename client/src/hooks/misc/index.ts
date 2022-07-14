6a9a12f223aec134d75e836873fb390970f7c2f3
davidcoderistov <davidcoderistov@gmail.com>
2022-07-14 13:12:31 +0200

    Add useSnackbarEffect custom hook

--- client/src/hooks/misc/index.ts
+++ client/src/hooks/misc/index.ts
@@ -12,3 +12,26 @@ export const useDidMountEffect = (callback: () => any, deps: Array<any>) => {
         }
     }, deps)
 }
+
+export const useSnackbarEffect = ({ isSnackbarOpen, isError, errorMessage, successMessage, setSnackbarOpen, setSnackbarError, setSnackbarMessage }: {
+    isSnackbarOpen: boolean,
+    isError: boolean,
+    errorMessage: string | null,
+    successMessage: string | null
+    setSnackbarOpen: (snackbarOpen: boolean) => void
+    setSnackbarError: (snackbarError: boolean) => void
+    setSnackbarMessage: (snackbarMessage: string) => void
+}) => {
+    useEffect(() => {
+        if (isSnackbarOpen) {
+            setSnackbarOpen(true)
+            const error = isError
+            setSnackbarError(error)
+            setSnackbarMessage(error ? errorMessage ? errorMessage : '' : successMessage ? successMessage : '')
+        } else {
+            setSnackbarOpen(false)
+            setSnackbarError(false)
+            setSnackbarMessage('')
+        }
+    }, [isSnackbarOpen])
+}
