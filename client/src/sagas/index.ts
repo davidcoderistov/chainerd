36f492716de83ec9c3595c591dc1d52005aa4dc2
davidcoderistov <davidcoderistov@gmail.com>
2022-07-14 11:33:31 +0200

    Add new address reducer and root saga

--- client/src/sagas/index.ts
+++ client/src/sagas/index.ts
@@ -1,11 +1,13 @@
 import { all } from 'redux-saga/effects'
 import watchKeystore from './keystore'
 import watchTransaction from './transaction'
+import watchAddress from './address'
 
 function *rootSaga() {
     yield all([
         watchKeystore(),
         watchTransaction(),
+        watchAddress(),
     ])
 }
 
--- client/src/slices/index.ts
+++ client/src/slices/index.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from '@reduxjs/toolkit'
 import keystoreReducer from './keystore'
+import addressReducer from './address'
 import transactionReducer from './transaction'
 
 const rootReducer = combineReducers({
     keystore: keystoreReducer,
+    address: addressReducer,
     transaction: transactionReducer,
 })
 
