f14f62008d12413e421be6a853972d6a7ec45fa4
davidcoderistov <davidcoderistov@gmail.com>
2022-07-20 21:49:52 +0200

    Add getEthPriceAt as coingecko service

--- client/src/services/coingecko.ts
+++ client/src/services/coingecko.ts
@@ -25,3 +25,24 @@ export function getEthPrice (): Promise<number> {
         }).catch(reject)
     })
 }
+
+export function getEthPriceAt (date: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+        axiosInstance.get('/coins/ethereum/history', {
+            params: {
+                date,
+            }
+        }).then(response => {
+            if (response && response.data && response.data.market_data && response.data.market_data.current_price) {
+                return resolve(response.data.market_data.current_price.usd)
+            }
+            return reject({
+                response: {
+                    data: {
+                        error: 'Cannot get eth price at the moment'
+                    }
+                }
+            })
+        }).catch(reject)
+    })
+}
