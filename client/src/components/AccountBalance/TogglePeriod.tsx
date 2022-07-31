9093fa8fa0975cfa104485a4f447fda788f72154
davidcoderistov <davidcoderistov@gmail.com>
2022-07-31 15:17:58 +0200

    Create TogglePeriod component

---
+++ client/src/components/AccountBalance/TogglePeriod.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { ToggleButtonGroup, ToggleButton } from '@mui/material'
+
+export interface TogglePeriodProps {
+    period: 'week' | 'month' | 'year'
+    onChange: (period: 'week' | 'month' | 'year') => void
+}
+
+export default function TogglePeriod ({ period, onChange }: TogglePeriodProps) {
+
+    const handleChange = (event: React.MouseEvent<HTMLElement>, period: 'week' | 'month' | 'year') => {
+        onChange(period)
+    }
+
+    return (
+        <ToggleButtonGroup
+            color='primary'
+            size='small'
+            value={period}
+            exclusive
+            onChange={handleChange}
+        >
+            <ToggleButton value='week'>1W</ToggleButton>
+            <ToggleButton value='month'>1M</ToggleButton>
+            <ToggleButton value='year'>1Y</ToggleButton>
+        </ToggleButtonGroup>
+    )
+}
