a1a297e8c4f9436a53720a0aa12b34f23a283229
davidcoderistov <davidcoderistov@gmail.com>
2022-08-04 12:51:55 +0200

    Create ViewAccountBalance component

---
+++ client/src/components/ViewAccountBalance/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Paper } from '@mui/material'
+import AccountBalance, { AccountBalanceProps } from '../AccountBalance'
+
+
+export default function ViewAccountBalance (props: AccountBalanceProps) {
+
+    return (
+        <Paper sx={{ padding: '30px' }} elevation={4}>
+            <AccountBalance
+                balance={props.balance}
+                chartData={props.chartData}
+                chartDataLoading={props.chartDataLoading}
+                fiat={props.fiat}
+                periodType={props.periodType}
+                height={props.height}
+                onChangeBalanceView={props.onChangeBalanceView}
+                onChangePeriod={props.onChangePeriod} />
+        </Paper>
+    )
+}
