2b985a8a53324f5ea3a7441123fca87c63c6c077
davidcoderistov <davidcoderistov@gmail.com>
2022-08-04 11:51:48 +0200

    Disable toggle period if loading

--- client/src/components/AccountBalance/TogglePeriod.tsx
+++ client/src/components/AccountBalance/TogglePeriod.tsx
@@ -4,9 +4,10 @@ import { ToggleButtonGroup, ToggleButton } from '@mui/material'
 export interface TogglePeriodProps {
     period: 'weekly' | 'monthly' | 'yearly'
     onChangePeriod: (period: 'weekly' | 'monthly' | 'yearly') => void
+    disabled: boolean
 }
 
-export default function TogglePeriod ({ period, onChangePeriod }: TogglePeriodProps) {
+export default function TogglePeriod ({ period, onChangePeriod, disabled }: TogglePeriodProps) {
 
     const handleChange = (event: React.MouseEvent<HTMLElement>, period: 'weekly' | 'monthly' | 'yearly' | null) => {
         if (period) {
@@ -21,6 +22,7 @@ export default function TogglePeriod ({ period, onChangePeriod }: TogglePeriodPr
             value={period}
             exclusive
             onChange={handleChange}
+            disabled={disabled}
         >
             <ToggleButton value='weekly'>1W</ToggleButton>
             <ToggleButton value='monthly'>1M</ToggleButton>
