d9d7eac4da9b168c98e36036bed5d0f75734c1d2
davidcoderistov <davidcoderistov@gmail.com>
2022-08-14 15:56:24 +0200

    Add getTransactionCount as a service

--- client/src/services/ethers.ts
+++ client/src/services/ethers.ts
@@ -3,3 +3,7 @@ import { ethersProvider } from '../providers'
 export function getEthBalance (address: string, blockNumber: string): Promise<string> {
     return ethersProvider.send('eth_getBalance', [address, blockNumber])
 }
+
+export function getTransactionCount (address: string): Promise<number> {
+    return ethersProvider.getTransactionCount(address)
+}
