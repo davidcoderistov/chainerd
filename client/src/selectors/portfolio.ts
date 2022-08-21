24784bd6585d4fa1ecc4b31250a6e542aa3f8e6d
davidcoderistov <davidcoderistov@gmail.com>
2022-08-21 13:40:04 +0200

    Add additional selectors for portfolio slice

--- client/src/selectors/portfolio.ts
+++ client/src/selectors/portfolio.ts
@@ -1,5 +1,9 @@
 import { RootState } from '../app/store'
+import { getAddresses } from './address'
 import { PeriodType, PortfolioPoints } from '../slices/portfolio'
+import { AddressType } from '../slices/address'
+import { Transaction as TableTransaction } from '../components/Transactions'
+import { Transaction as ModalTransaction } from '../components/TransactionDetailsModal'
 
 export function getChartDataByAddress (state: RootState) {
     return state.portfolio.chartDataByAddress
@@ -67,4 +71,71 @@ export function getIsChartDataLoadingByPortfolioAddress (state: RootState): bool
         }
     }
     return false
+}
+
+export function getLatestTransactions (state: RootState) {
+    return state.portfolio.latestTransactions.data
+}
+
+export function getLatestTransactionsLoading (state: RootState) {
+    return state.portfolio.latestTransactions.loading
+}
+
+export function getLatestTransactionsFetched (state: RootState) {
+    return state.portfolio.latestTransactions.fetched
+}
+
+type TransactionsData = {
+    table: TableTransaction[]
+    modal: ModalTransaction[]
+}
+
+export function getLatestTransactionsData (state: RootState) {
+    const transactions = getLatestTransactions(state)
+    const addresses = getAddresses(state)
+    const addressesMapping: {
+        [address: string]: AddressType
+    } = addresses.reduce((mapping, address) => ({
+        ...mapping,
+        [address.address.trim().toLowerCase()]: address
+    }), {})
+    return transactions.reduce((transactions: TransactionsData, transaction) => {
+        const from = transaction.from
+        const to = transaction.to
+        const hasFrom = addressesMapping.hasOwnProperty(from)
+        const hasTo = addressesMapping.hasOwnProperty(to)
+        const withdrawal = hasFrom ? true : hasTo ? false : false
+        const selectedAddress = hasFrom ? addressesMapping[from] : hasTo ? addressesMapping[to] : null
+        const selectedAccount = selectedAddress ? selectedAddress.alias ? selectedAddress.alias : 'N/A' : 'N/A'
+        return {
+            table: [
+                ...transactions.table,
+                {
+                    withdrawal,
+                    hash: transaction.hash,
+                    from,
+                    to,
+                    date: transaction.timestamp,
+                    value: transaction.value,
+                    amount: transaction.amount
+                }
+            ],
+            modal: [
+                ...transactions.modal,
+                {
+                    withdrawal,
+                    ethAmount: transaction.value,
+                    fiatAmount: transaction.amount,
+                    account: selectedAccount,
+                    timestamp: transaction.timestamp,
+                    fee: transaction.fee,
+                    status: transaction.status,
+                    blockNumber: transaction.blockNumber,
+                    transactionHash: transaction.hash,
+                    from,
+                    to,
+                }
+            ]
+        }
+    }, { table: [], modal: [] })
 }
