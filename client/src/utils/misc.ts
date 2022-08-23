fccd2dd8d31302e0dfda96bbf49337ffd17fcecf
davidcoderistov <davidcoderistov@gmail.com>
2022-08-23 09:28:17 +0200

    Add getAgo as utils method

--- client/src/utils/misc.ts
+++ client/src/utils/misc.ts
@@ -1,6 +1,7 @@
 import _reduce from 'lodash/reduce'
 import _sortBy from 'lodash/sortBy'
 import _sum from 'lodash/sum'
+import moment from 'moment'
 
 export function getAxiosErrorMessage (error: any, defaultMessage: string): string {
     return error && error.response && error.response.data && error.response.data.error ?
@@ -41,4 +42,24 @@ export function distributePercentages (numbers: number[]) {
         return distribute(percentages)
     }
     return numbers
+}
+
+export function getAgo (timestamp: string): string {
+    const now = moment()
+    const start = moment.unix(Number(timestamp))
+    if (now.diff(start, 'minutes') < 1) {
+        const seconds = now.diff(start, 'seconds')
+        return `${seconds} ${seconds > 1 ? 'seconds' : 'second'} ago`
+    } else if (now.diff(start, 'hours') < 1) {
+        const minutes = now.diff(start, 'minutes')
+        return `${minutes} ${minutes > 1 ? 'minutes' : 'minute'} ago`
+    } else if (now.diff(start, 'days') < 1) {
+        const hours = now.diff(start, 'hours')
+        return `${hours} ${hours > 1 ? 'hours' : 'hour'} ago`
+    } else if (now.diff(start, 'weeks') < 1) {
+        const days = now.diff(start, 'days')
+        return `${days} ${days > 1 ? 'days' : 'day'} ago`
+    } else {
+        return start.format('LLL')
+    }
 }
