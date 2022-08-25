05c571bd5e066c9ddcdb6b6e4e2ded1ccecbd5c5
davidcoderistov <davidcoderistov@gmail.com>
2022-08-25 16:13:46 +0200

    Main App component setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { ThemeContext, themes } from './config'
+import { BrowserRouter as Router } from 'react-router-dom'
+import HomePage from './pages/HomePage'
 
 function App() {
+
+  const [darkTheme, setTheme] = useState<boolean>(true)
+
+  const handleChangeTheme = (dark: boolean) => {
+      setTheme(dark)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <ThemeContext.Provider value={{ theme: darkTheme ? themes.dark : themes.light, changeTheme: handleChangeTheme }}>
+          <Router>
+              <HomePage />
+          </Router>
+      </ThemeContext.Provider>
+  )
 }
 
-export default App;
+export default App
