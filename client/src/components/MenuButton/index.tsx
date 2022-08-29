04c28edf0277daddd97ab6f3293bc9753e9c93f6
davidcoderistov <davidcoderistov@gmail.com>
2022-08-29 13:59:46 +0200

    Create MenuButton component

---
+++ client/src/components/MenuButton/index.tsx
@@ -0,0 +1,65 @@
+import React, { useState, useCallback } from 'react'
+import { Button, Menu, MenuItem } from '@mui/material'
+import { KeyboardArrowDown, KeyboardArrowUp } from '@mui/icons-material'
+
+
+interface MenuButtonProps {
+    children: any
+    options: string[]
+    disabled: boolean
+    onChange: (option: string, index: number) => void
+}
+
+export default function MenuButton ({ children, options, disabled, onChange }: MenuButtonProps) {
+
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+    const open = Boolean(anchorEl)
+
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget)
+    }
+
+    const handleClose = () => {
+        setAnchorEl(null)
+    }
+
+    const handleChange = useCallback(
+        (option: string, index: number) => {
+            onChange(option, index)
+            handleClose()
+        },
+        [onChange]
+    )
+
+    return (
+        <React.Fragment>
+            <Button
+                sx={{ textTransform: 'none' }}
+                aria-haspopup='true'
+                aria-expanded={ open ? 'true' : undefined }
+                disabled={disabled}
+                onClick={handleClick}
+                endIcon={open ? <KeyboardArrowUp /> : <KeyboardArrowDown />}
+            >
+                { children }
+            </Button>
+            <Menu
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'right',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+            >
+                { options.map((option, index) => (
+                    <MenuItem key={index} onClick={() => handleChange(option, index)}>{ option }</MenuItem>
+                ))}
+            </Menu>
+        </React.Fragment>
+    )
+}
