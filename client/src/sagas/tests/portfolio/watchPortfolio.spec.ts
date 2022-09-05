f1b00225741afa24f747ddc0cf3cf65f65e89116
davidcoderistov <davidcoderistov@gmail.com>
2022-09-05 12:55:17 +0200

    Create unit tests for *watchPortfolio saga

---
+++ client/src/sagas/tests/portfolio/watchPortfolio.spec.ts
@@ -0,0 +1,48 @@
+import { takeLatest } from 'redux-saga/effects'
+import watchPortfolio, {
+    generateWeeklyData,
+    generateMonthlyData,
+    generateYearlyData,
+    fetchLatestTransactions,
+} from '../../portfolio'
+import { portfolioActions } from '../../../slices/portfolio'
+
+
+describe('Test *watchPortfolio saga', () => {
+    const it = watchPortfolio()
+
+    test('*watchPortfolio should watch for *generateWeeklyData saga', () => {
+        expect(it.next()).toEqual({
+            value: takeLatest(portfolioActions.fetchWeekly.type, generateWeeklyData),
+            done: false,
+        })
+    })
+
+    test('*watchPortfolio should watch for *generateMonthlyData saga', () => {
+        expect(it.next()).toEqual({
+            value: takeLatest(portfolioActions.fetchMonthly.type, generateMonthlyData),
+            done: false,
+        })
+    })
+
+    test('*watchPortfolio should watch for *generateYearlyData saga', () => {
+        expect(it.next()).toEqual({
+            value: takeLatest(portfolioActions.fetchYearly.type, generateYearlyData),
+            done: false,
+        })
+    })
+
+    test('*watchPortfolio should watch for *fetchLatestTransactions saga', () => {
+        expect(it.next()).toEqual({
+            value: takeLatest(portfolioActions.fetchLatestTransactions, fetchLatestTransactions),
+            done: false,
+        })
+    })
+
+    test('*watchPortfolio should terminate', () => {
+        expect(it.next()).toEqual({
+            value: undefined,
+            done: true,
+        })
+    })
+})
